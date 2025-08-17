c32e3fada1e10ed19a13769518862180fd0fd902
Jahidul Islam <jahidulzaid@gmail.com>
2025-08-17 22:49:31 +0600

    cleanup py

--- prompt.py
+++ prompt.py
@@ -2,9 +2,9 @@
 # ## Step 0 - Installing Librariess
 
 # %%
-!pip install -q --no-deps bitsandbytes accelerate xformers==0.0.29.post3 peft trl triton cut_cross_entropy unsloth_zoo
-!pip install -q sentencepiece protobuf "datasets>=3.4.1" huggingface_hub hf_transfer
-!pip install -q --no-deps unsloth
+pip install --no-deps bitsandbytes accelerate xformers==0.0.29.post3 peft trl triton cut_cross_entropy unsloth_zoo
+pip install sentencepiece protobuf "datasets>=3.4.1" huggingface_hub hf_transfer
+pip install --no-deps unsloth
 
 # %% [markdown]
 # ## Step 1 - Choosing Models
