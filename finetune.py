a11191f2557b3a1810bd87b1c545df4ce5ab2608
Jahidul Islam <jahidulzaid@gmail.com>
2025-08-18 19:58:21 +0600

    finetune

--- finetune.py
+++ finetune.py
@@ -1,10 +1,4 @@
-# %% [markdown]
-# ## Step 0 - Installing Librariess
 
-# %%
-pip install -q --no-deps bitsandbytes accelerate xformers==0.0.29.post3 peft trl triton cut_cross_entropy unsloth_zoo
-pip install -q sentencepiece protobuf "datasets>=3.4.1" huggingface_hub hf_transfer
-pip install -q --no-deps unsloth
 
 # %% [markdown]
 # ## Step 1 - Choose a Base Model
