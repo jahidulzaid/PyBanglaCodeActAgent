2630944af6c55b7d2be2ac5c2245329fec05ad99
Jahidul Islam <jahidulzaid@gmail.com>
2025-09-12 02:54:49 +0600

    model para

--- test_phase/blp-codeV14.0.6_DeepSeek-Coder-V2-Lite-Instruct.py
+++ test_phase/blp-codeV14.0.6_DeepSeek-Coder-V2-Lite-Instruct.py
@@ -15,7 +15,7 @@ model = "codellama/CodeLlama-13b-Python-hf"
 llm = vllm.LLM(
     model,
     # quantization="awq",
-    max_model_len=8192,
+    max_model_len=6500,
     enable_prefix_caching=True,
     tensor_parallel_size=torch.cuda.device_count(),
 )
@@ -29,7 +29,7 @@ def llm_engine(messages, stop_sequences=None, start_sequence=None) -> str:
         # use_beam_search=True,
         # num_beams=3,
         best_of=1,
-        max_tokens=8192,
+        max_tokens=6500,
         stop=stop_sequences,
         include_stop_str_in_output=True,
     )
